157d363b09082f8f979d300953db53a1280dfb5c
sejin-chun <101098015+sejin-chun@users.noreply.github.com>
2024-03-27 02:22:28 +0000

    add codes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,8 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import altair as alt
 
-# Page title
-st.set_page_config(page_title='Interactive Data Explorer', page_icon='📊')
-st.title('📊 Interactive Data Explorer')
-
-st.title ("this is the app title")
-st.header("this is the markdown")
-st.markdown("this is the header")
-st.subheader("this is the subheader")
-st.caption("this is the caption")
-st.code("x=2021")
-st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+rand=np.random.normal(1, 2, size=20)
+fig, ax = plt.subplots()
+ax.hist(rand, bins=15)
+st.pyplot(fig)
