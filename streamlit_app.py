e4ff4c5009c2ba5d0aee2ecf7aa16aef9b73f0ee
sejin-chun <101098015+sejin-chun@users.noreply.github.com>
2024-03-27 01:58:13 +0000

    add texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,11 @@ import altair as alt
 # Page title
 st.set_page_config(page_title='Interactive Data Explorer', page_icon='📊')
 st.title('📊 Interactive Data Explorer')
+
+st.title ("this is the app title")
+st.header("this is the markdown")
+st.markdown("this is the header")
+st.subheader("this is the subheader")
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
